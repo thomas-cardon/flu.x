46f3478f04365afd492f3c41c3fcb3576e68e1f1
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-10 15:42:31 +0200

    Renamed manaflux-server to flu.x

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   "port": process.env.PORT || 8920,
   "host": "0.0.0.0",
-  "db_url": process.env.DB_URL,
-  "password": process.env.PASSWORD,
+  "db_url": "mongodb://manaflux:2v4R8bKCwcRB69s@ds115613.mlab.com:15613/manaflux-server",
+  "password": "SAzPgU4x3d",
   "env": process.argv.includes('--dev') ? 'development' : 'production'
 }
