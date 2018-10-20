a8c69bed29ded10e1532e053f6d7ae32eb73518e
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-20 13:34:56 +0200

    Removed settings.js from security reasons
    An issue with GitHub desktop made a commit including the settings.js that had passwords (since changed) to the database and dashboard, etc.

--- settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "port": process.env.PORT || 8920,
-  "host": "0.0.0.0",
-  "db_url": "mongodb://manaflux:2v4R8bKCwcRB69s@ds115613.mlab.com:15613/manaflux-server",
-  "password": "SAzPgU4x3d",
-  "env": process.argv.includes('--dev') ? 'development' : 'production'
-}
