8857eceedfe58ccdaf9f475ad98890565bab00b4
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-20 13:19:00 +0200

    Started replacing Hapi by Express

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
 'use strict';
 
 const path = require('path');
-const Hapi = require('hapi'), Hoek = require('hoek');
 
 const Settings = require('./settings');
-const Routes = require('./lib/routes');
 
-const { PerkPage, SummonerSpellList, ItemSet, Block } = require('./lib/models');
+const { PerkPage, SummonerSpellList, ItemSet, Block, User } = require('./lib/models');
+const Login = require('./lib/helpers/Login');
 
 const mongoose = require('mongoose');
 
-const server = Hapi.server({
-    port: Settings.port,
-    host: Settings.host,
-    debug: Settings.env === 'development' ? { request: ['error'] } : {}
+const express = require('express'), compression = require('compression');
+
+const app = express();
+
+mongoose.connect(Settings.db_url, { useNewUrlParser: true });
+
+app.use(session({
+  secret: Settings.secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: Settings.env === 'production' }
+}));
+
+app.use(compression());
+app.use('/', express.static('static/public'));
+
+app.use('/data', require('./lib/controllers/Package'));
+app.use('/', require('./lib/controllers/Misc'));
+app.use('/', require('./lib/controllers/Dashboard'));
+
+app.set('views','./lib/views');
+app.set('view engine', 'pug');
+
+app.listen(Settings.port, function () {
+  console.log(`Flu.x running on port ${Settings.port}`);
 });
-
-const init = async () => {
-  mongoose.connect(Settings.db_url, { useNewUrlParser: true });
-  await server.register([ require('vision'), require('inert') ]);
-
-  // View settings
-  server.views({
-    engines: { pug: require('pug') },
-    path: path.join(__dirname, 'lib/views'),
-    helpersPath: path.join(__dirname, 'lib/helpers'),
-    compileOptions: {
-      pretty: false
-    },
-    isCached: Settings.env === 'production'
-  });
-
-  // Add routes
-  server.route(Routes);
-
-  await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
-};
 
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
 });
-
-init();
