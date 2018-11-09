11f882e3c94df1f0932532923140337e7ed9f524
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-11-09 23:20:57 +0100

    Added CORS support

--- server.js
+++ server.js
@@ -32,6 +32,13 @@ app.use(compression());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', express.static('static/public'));
 
 app.use('/data', require('./lib/controllers/Package'));
