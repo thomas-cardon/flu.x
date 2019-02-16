adcdb849fd61b459d42f5b205af157b23c9713fd
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-02-16 12:32:12 +0100

    Fixed upload not working

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.use(compression());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -70,6 +69,9 @@ app.use(function(err, req, res, next) {
 
 app.set('views','./lib/views');
 app.set('view engine', 'pug');
+
+require('./lib/helpers/Package')._isValidVersion = (...x) => VersionChecker.isValidVersion(...x);
+console.dir(require('./lib/helpers/Package'));
 
 VersionChecker.loadVersions().then(() => {
   VersionChecker.searchForOutdatedData();
